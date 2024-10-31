3074779ebba957130043d8fbf8c82abe3749e42b
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-10-31 17:01:26 +0600

    little update

--- src/app/middleware/globalErrorHandlear.ts
+++ src/app/middleware/globalErrorHandlear.ts
@@ -10,7 +10,6 @@ import { AppError } from '../errors/AppErrors';
 
 // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  // ==================================>>>>>>>>> settings default values
   let statusCode = 500;
   let message = 'Somthing went wrong!';
   let errorSources: TErrorSource = [
@@ -59,7 +58,6 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     ];
   }
 
-  // ==================================>>>>>>>>>  ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
