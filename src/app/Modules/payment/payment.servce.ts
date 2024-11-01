e3371fd744fa161c494c114bb527e3a4868e0959
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-11-01 15:06:55 +0600

    clean code

--- src/app/Modules/payment/payment.servce.ts
+++ src/app/Modules/payment/payment.servce.ts
@@ -11,11 +11,6 @@ paypal.configure({
     'client_id': 'AUHCLLlrN0fUteHTIYiBX7ZOoduVvF0mp4QSDUQOf_m2GohS_kVr6z8CbTJgOMnGNyMAiLsx_EWf8l5C',
     'client_secret': 'EDwN_-iERzhwDKJJ4x84VM3V03dWv4laHpF21fnDvmPup5a-PWvv8poGghba6XvtydL0V04iwjIag63z'
 });
-// paypal.configure({
-//     'mode': 'sandbox', 
-//     'client_id': 'AeMnBMlrboT2yZ77Ny1Zuwm-UnhJeeMzvE1D1ana1ZetUAzPfo7C-Px41iR4FijH5SN1FHEYrGokg3G2',
-//     'client_secret': 'EJkWItnQryzrdRMgIyVw8oJ1tgQBpWCe7EVTdoI2_EqLVIWO-xI_mX40IfFX6DUUxPLMIs6Sy2r2SVfr'
-// });
 
 const paymentFun = (amount: TPaymentAmount): Promise<{ forwardLink: string }> => {
 
@@ -69,12 +64,10 @@ const executePaymentFun = (orderID : string, payerID : string) => {
     });
 };
 
-// const stripe = new Stripe('sk_test_51NFvq6ArRmO7hNaVBU6gVxCbaksurKb6Sspg6o8HePfktRB4OQY6kX5qqcQgfxnLnJ3w9k2EA0T569uYp8DEcfeq00KXKRmLUw');
 const stripe = new Stripe('sk_test_51QFpATLEvlBZD5dJjsneUWfIN2W2ok3yfxHN7qyLB2TRPYn0bs0UCzWytfZgZwrpcboY5GXMyen4BwCPthGLCrRX001T5gDgLK');
 
 
 const stripePayment = async (amount: number) => {   
-
     try {
         const paymentIntent = await stripe.paymentIntents.create({
             amount: amount  * 100, 
