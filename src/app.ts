0b6ed02e566cb804539bf711a739fd5ea97deaa4
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-11-09 17:42:29 +0600

    payment update code

--- src/app.ts
+++ src/app.ts
@@ -56,21 +56,24 @@ app.get('/subscribe', async (req, res) => {
 
 app.get('/success', async (req, res) => {
   const session = await stripe.checkout.sessions.retrieve(req.query.session_id, { expand: ['subscription', 'subscription.plan.product'] });
-  // console.log(62, "subscription data:", session?.subscription);
-  console.log(60, "session_id", req.query.session_id);
-
-  console.log(62, "session_id", session?.subscription);
-  console.log(63, "subscription data:", session?.subscription?.id);
-  console.log(64, "subscription country :", session?.customer_details?.address?.country);
-  console.log(65, "subscription country email:", session?.customer_details?.email);
+  // console.log(59, "session data:", session);
+  console.log(60, "session_id = ", req.query.session_id);
+  console.log(61, "subscription = ", session?.subscription);
+  console.log(62, "customer_id = ", session?.subscription?.customer);
+  console.log(63, "subscription data: = ", session?.subscription?.id);
+  console.log(64, "subscription country : = ", session?.customer_details?.address?.country);
+  console.log(65, "subscription country email: = ", session?.customer_details?.email);
+  console.log(66, "customer name = ", session?.customer_details.name );
+  console.log(67, "plan = ", (session?.subscription.plan.amount / 100));
+  console.log(68, "status = ", session?.subscription.status);
 
   // const successData = {
   //   subscription : session?.subscription,
   // }
-
   // console.log(69, successData);
   
-  res.send('Subscribed successfully');
+  // res.send('Subscribed successfully');
+  res.send({status : session?.subscription.status});
 });
 
 app.get('/cancel', (req, res) => {
@@ -87,6 +90,56 @@ app.get("/customers/:customerId", async(req, res)=>{
   
   res.redirect(portalSession.url)
 } )
+
+
+
+
+app.post('/webhook', express.raw({type: 'application/json'}), (req, res) => {
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+      event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET_KEY);
+  } catch (err) {
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Handle the event
+  switch (event.type) {
+    //Event when the subscription started
+    case 'checkout.session.completed':
+      console.log('New Subscription started!')
+      console.log(event.data)
+      break;
+
+    // Event when the payment is successfull (every subscription interval)  
+    case 'invoice.paid':
+      console.log('Invoice paid')
+      console.log(event.data)
+      break;
+
+    // Event when the payment failed due to card problems or insufficient funds (every subscription interval)  
+    case 'invoice.payment_failed':  
+      console.log('Invoice payment failed!')
+      console.log(event.data)
+      break;
+
+    // Event when subscription is updated  
+    case 'customer.subscription.updated':
+      console.log('Subscription updated!')
+      console.log(event.data)
+      break
+
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.send();
+});
+
+
+
 
 export default app;
 // cus_RBAjle507vPOhf
