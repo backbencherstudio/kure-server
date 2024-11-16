7e745905d65c70c57341a5e9644fdb74583c2fa1
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-11-16 17:19:45 +0600

    working on session file , for validatio

--- dist/app/config/index.js
+++ dist/app/config/index.js
@@ -16,5 +16,9 @@ exports.default = {
     jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
     jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
     email_pass: process.env.EMAIL_PASS,
-    email_port: process.env.EMAIL_PORT
+    email_port: process.env.EMAIL_PORT,
+    client_base_url: process.env.CLIENT_BASE_URL,
+    stripe_test_secret_key: process.env.STRIPE_TEST_SECRET_KEY,
+    stripe_live_secret_key: process.env.STRIPE_LIVE_SECRET_KEY,
+    stripe_webhook_secret_key: process.env.STRIPE_WEBHOOK_SECRET_KEY,
 };
